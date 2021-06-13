37179d4bb87750f153b854b2ec9e112ea999026b
ADIL KARIM <adilkarimscience@gmail.com>
2021-06-13 22:29:41 +0100

    First Commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -20,7 +20,6 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
